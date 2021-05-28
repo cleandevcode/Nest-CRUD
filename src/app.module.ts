93b33aa325f353c9d8215ff37a5800e06aa33898
Superstar-IT <freewebdev0213@gmail.com>
2021-05-28 09:18:10 +0800

    create module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,40 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as OrmConfig from './orm.config';
 
 import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { ClientModule } from './client/client.module';
-import { SeedModule } from './seed/seed.module';
-import { ClinicModule } from './clinic/clinic.module';
-import { ClinicianModule } from './clinician/clinician.module';
-import { ProductModule } from './product/product.module';
-import { ClaimModule } from './claim/claim.module';
-import { InsurerModule } from './insurer/insurer.module';
-import { ActionModule } from './action/action.module';
-import { SearchModule } from './search/search.module';
-import { AdjudicatorModule } from './adjudicator/adjudicator.module';
-import { AdjudicationModule } from './adjudication/adjudication.module';
-import { ClaimResponseModule } from './claim-response/claim-response.module';
-import { ConditionModule } from './condition/condition.module';
+import { StudentModule } from './student/student.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(OrmConfig),
-    UserModule,
-    AuthModule,
-    ClientModule,
-    SeedModule,
-    ClinicModule,
-    ClinicianModule,
-    ProductModule,
-    ClaimModule,
-    ConditionModule,
-    InsurerModule,
-    ActionModule,
-    SearchModule,
-    AdjudicatorModule,
-    AdjudicationModule,
-    ClaimResponseModule,
+    StudentModule,
+    CourseModule,
   ],
   controllers: [AppController],
 })
